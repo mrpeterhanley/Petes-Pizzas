da744c6f0a879c29310e1de9fc46fc6cec343adb
Peter Hanley <hanley.peter@gmail.com>
2020-12-09 18:32:16 +0800

    Added Slicemasters page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,10 +1,84 @@
 import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
 
-export default function SlicemastersPage() {
+const SlicemasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    position: relative;
+    z-index: 2;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
+export default function SlicemastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
   return (
     <Layout>
-      <p>Slicemasters page</p>
+      <SlicemasterGrid>
+        {slicemasters.map((person) => (
+          <SlicemasterStyles>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </SlicemasterStyles>
+        ))}
+      </SlicemasterGrid>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query MyQuery {
+    slicemasters: allSanityPerson {
+      nodes {
+        name
+        description
+        id
+        slug {
+          current
+        }
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`;
