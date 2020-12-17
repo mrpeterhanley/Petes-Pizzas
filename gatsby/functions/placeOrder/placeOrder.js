a03a92179e1c9feab1db2ce8d467a5512af341b2
Peter Hanley <hanley.peter@gmail.com>
2020-12-17 13:13:15 +0800

    Added netlify serverless functions

---
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+exports.handler = async (event, context) => {
+  const info = await transporter.sendMail({
+    from: "Pete's Pizzas <hanley.peter@gmail.com>",
+    to: 'hanley.pedro@gmail.com',
+    subject: 'New order!',
+    html: `<p>Your new pizza order is here!</p>`,
+  });
+  console.log(info);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(info),
+  };
+};
