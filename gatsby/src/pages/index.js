bbfec0088c41fe1b2316406d8f63dbf0a1ad434e
Peter Hanley <hanley.peter@gmail.com>
2020-12-18 16:25:33 +0800

    Added some home page content

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
+
+const IndexStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1,
+  p {
+    text-align: center;
+  }
+`;
 
 export default function HomePage() {
   return (
     <Layout>
-      <p>This is the beginning of my pizza website!</p>
+      <IndexStyles>
+        <h1>🍕 🍕 The Best Tasting Pizzas You'll Never Eat! 🍕 🍕</h1>
+        <p>Open 11am to 11pm Every Single Day</p>
+      </IndexStyles>
     </Layout>
   );
 }
