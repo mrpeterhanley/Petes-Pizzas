d797033f802a7f4f2b25385eb81e42c678bf2feb
Peter Hanley <hanley.peter@gmail.com>
2020-12-23 19:57:56 +0800

    Adjusted footer styling on mobile

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -8,6 +8,12 @@ const FooterStyles = styled.footer`
   p {
     margin-bottom: 0;
   }
+
+  @media (max-width: 600px) {
+    p {
+      font-size: 1.3rem;
+    }
+  }
 `;
 
 export default function Footer() {
